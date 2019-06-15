7f14ca9d27f8982752fa028db77665e84beb2834
cgewecke <christophergewecke@gmail.com>
2019-06-15 13:39:44 -0700

    Debug codechecks.js

--- codechecks.js
+++ codechecks.js
@@ -3,8 +3,9 @@ const fs = require("fs");
 const { codechecks } = require("@codechecks/client");
 
 module.exports.main = async function main() {
+  console.log("Running main");
   if (!codechecks.isPr()) return;
-
+  console.log("Post PR check");
   const report = JSON.parse(fs.readFileSync("codechecksReport.json", "utf-8"));
 
   await codechecks.saveValue(report.namespace, report.newData);
